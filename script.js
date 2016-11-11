d344a8512f9e4bf1f0da14372e2f2c4b542df4f0
Hugo Fonseca <hugo.fonseca@beubi.com>
2016-11-11 16:26:05 +0000

    use click cause ...

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    var r = $('<input id="hide-top" type="button" value="Toggle This" style="margin: 4px 2em;"/>');
+    $('div.aui-item.section-title').css("display", " -webkit-inline-box");
+    $('div.aui-item.section-title').append(r);
 
+    $("#hide-top").on("click", function () {
+        $("h1.filename").on("click", function () {
+            var filename = $(this).text().replace('File', '').trim();
+            $("section[data-identifier='" + filename + "'] > div > div.diff-content-container.refract-container.word-diff").toggle();
+        });
+        $("#pull-request-header").toggle();
 
-	$("#diff-tab-preload").hide();
+        $("ul.commit-files-summary").toggle();
+        $('div.diff-content-container.refract-container.word-diff').toggle();
 
-	$( "section" ).each(function( index ) {
-	  $(this).css( "margin-top", "0px" );
-		$(this).css( "min-height", "0px" );
-	});
+        $("h1.filename").each(function () {
+            $(this).css("color", "black");
+            $(this).css("font-weight", "bold");
+        });
+        $("#diff-tab-preload").toggle();
 
-	var r= $('<input id="hide-top" type="button" value="Toggle This" style="margin: 4px 2em;"/>');
-	$('div.aui-item.section-title').css("display", " -webkit-inline-box");
-	$('div.aui-item.section-title').append(r)
-
-  $("ul.commit-files-summary").hide();
-
-	$('div.diff-content-container.refract-container.word-diff').hide();
-
-	$( "h1.filename" ).each(function() {
-	  $(this).css( "color", "black" );
-		$(this).css( "font-weight", "bold" );
-	});
-
-	$("#hide-top").on( "click", function() {
-		$("#pull-request-header").toggle();
-	});
-	$( "h1.filename" ).on( "click", function() {
-		console.log('click');
-		var filename = $(this).text().replace('File','').trim();
-		$("section[data-identifier='"+filename+"'] > div > div.diff-content-container.refract-container.word-diff").toggle();
-	});
+        $("section").each(function () {
+            $(this).css("margin-top", "0px");
+            $(this).css("min-height", "0px");
+        });
+    });
 
 });
